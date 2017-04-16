c5f707bc61ac438c9a228c8ba0f2319b0ccc0774
SHUBHAM GOKHALE <shubhamgokhale1995@gmail.com>
2017-04-16 20:04:47 +0530

    DictionaryV2.py
    Show sub definitions along with examples.

--- Dictionary.py
+++ Dictionary.py
@@ -1,6 +1,6 @@
 import requests,json,sys
 p = input("Enter word to be searched.")
-url = "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" #APIs provided by oxford
+url = "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/"
 url+=p
 
 headers = {
@@ -19,21 +19,34 @@ if  response.headers['Content-Type'] == 'text/html':
     
 
 d = json.loads(response.text)
-tvara = 'Word is: '+d['results'][0]['id']
-print (tvara)
-tvara = "Pronounciation: "+d["results"][0]['lexicalEntries'][0]['pronunciations'][0]['phoneticSpelling']
+print ('Word is: '+d['results'][0]['id'])
 
 #Phonetic Spelling
-print(tvara)
+print("Pronounciation: "+d['results'][0]['lexicalEntries'][0]['pronunciations'][0]['phoneticSpelling'])
 cnt1=1
 for ia in range(0,len(d['results'][0]['lexicalEntries'])):
-    for ib in range(0,len(d['results'][0]['lexicalEntries'][ia]['entries'])):
-        for ic in range(0,len(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'])):
-            for idd in range(0,len(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['definitions'])):
-                tvara = "Definition "+str(cnt1)
-                print(tvara)
-                tvara = d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['definitions'][idd] 
-                print(tvara)
-                cnt1+=1
-
-                
+    if('entries' in d['results'][0]['lexicalEntries'][ia]):
+        for ib in range(0,len(d['results'][0]['lexicalEntries'][ia]['entries'])):
+            if('senses' in d['results'][0]['lexicalEntries'][ia]['entries'][ib]):
+                for ic in range(0,len(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'])):
+                    print()
+                    if('definitions' in d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]):
+                        for idd in range(0,len(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['definitions'])):
+                            print("Definition "+str(cnt1))
+                            print(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['definitions'][idd])
+                            cnt1+=1
+                    if('examples' in d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]):
+                        for idd in range(0,len(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['examples'])):
+                            print("Example")
+                            print(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['examples'][idd]['text'])
+                    if('subsenses' in d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]):
+                        for idd in range(0,len(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['subsenses'])):
+                            if('definitions' in d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['subsenses'][idd]):
+                                for ie in range(0,len(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['subsenses'][idd]['definitions'])):
+                                    print()
+                                    print("Sub-definition")
+                                    print(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['subsenses'][idd]['definitions'][ie])
+                            if('examples' in d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['subsenses'][idd]):
+                                for ie in range(0,len(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['subsenses'][idd]['examples'])):
+                                    print("Example")
+                                    print(d['results'][0]['lexicalEntries'][ia]['entries'][ib]['senses'][ic]['subsenses'][idd]['examples'][ie]['text'])
